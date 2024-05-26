d0ba1391388173a5cad7fda0660f16a9560d2686
Hung <huynhhung2108@gmail.com>
2024-05-26 17:45:10 +0700

    add Destroy Task

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -23,5 +23,11 @@ class TaskController extends Controller
         return new TaskResource($task);
     }
 
+    public  function destroy(Request $request,Task $task)
+    {
+        $task->delete();
+        return response()->noContent();
+    }
+
 
 }
